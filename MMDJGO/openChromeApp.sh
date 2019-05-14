9a4b64b02acaa22a1fed8af9f6f3097f8c1ace7d
Vahe Grigoryan <vg16661@essex.ac.uk>
2019-05-14 20:59:29 +0100

    Switching UI from GTK to HTML with Django

---
+++ MMDJGO/openChromeApp.sh
@@ -0,0 +1,2 @@
+chromium --incognito --kiosk --noerrdialogs --disable-translate --disable-cache --disk-cache-dir=/dev/null --disk-cache-size=1 --size=500 --app=http://localhost:8000
+
---
+++ MMDJGO/openWebSample.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+url = 'https://stackoverflow.com'
+
+webbrowser.open(url, 1)
