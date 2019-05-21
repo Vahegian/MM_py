f6ba22ad90014b81bc1ac58ead21f9160d491d23
Vahegian <vahegian@gmail.com>
2019-05-21 20:37:58 +0100

    simple changes to get a chrome window showing

--- constants/fileconsts.py
+++ constants/fileconsts.py
@@ -1,2 +1,3 @@
 pairsFile = '/home/lavaguiny/projects/Python/MM_py/raw/pairs.csv'
 uiFile = "/home/lavaguiny/projects/Python/MM_py/UI/mm.glade"
+thFile = "/home/lavaguiny/projects/Python/MM_py/raw/thInfo.csv"
--- mm_main.py
+++ mm_main.py
@@ -2,16 +2,30 @@
 # import os
 # print("The Dir >>>>>>>: ",  os.curdir)
 
-from uiControl.window_manager import WindowGer
+#from uiControl.window_manager import WindowGer
+from subprocess import call
 from binanceAPI.binanceCom import BinanceCom
 from IO.fileIO import FileIO
-from communicator.organiser import Organiser
+# from communicator.organiser import Organiser
+import time
+import threading
+
+
+def startServer():
+    sr = call("python3 ./MMDJGO/manage.py runserver", shell=True)
 
 if __name__ == "__main__":
+    stThread = threading.Thread(target=startServer)
+    stThread.deamon = True
+    stThread.start()
+  
     fio = FileIO()
     bc = BinanceCom()
-    wg = WindowGer()
-    oser = Organiser(fio, bc, wg)
+    #wg = WindowGer()
+    #oser = Organiser(fio, bc, wg)
+    #wg.startGTK()
     
+    time.sleep(2)
+    browser = call("./MMDJGO/openChromeApp.sh", shell=True)
 
 
